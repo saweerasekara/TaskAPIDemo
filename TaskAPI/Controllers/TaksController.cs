7a4c9a31647901bb9d48e4e9b2288d1cb2709d4d
Sachin Akalanka <s.akalanka@sana-commerce.com>
2024-01-05 00:32:03 +0530

    Add project files.

---
+++ TaskAPI/Controllers/TaksController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TaksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var task =  new string[] { "Task1", "Task2", "Task3" };
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public IActionResult NewTask()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTask()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTask()
+        {
+            return Ok();
+        }
+    }
+}
